1b7ddb6fdb16351a0ab2a06f4b6610da10e5acda
arizaxel16 <arizaxel16@gmail.com>
2024-10-28 19:47:54 -0500

    Separate auth into controller and service files
    Must check GOODPRACTICES

--- core/src/main/java/com/core/controller/AuthController.java
+++ core/src/main/java/com/core/controller/AuthController.java
@@ -1,27 +1,25 @@
 package com.core.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import com.core.controller.dto.UserCredentials;
+import com.core.service.AuthService;
 
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin(origins = "*")
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody UserCredentials userCredentials) {
-        // Log the received credentials for debugging
-        System.out.println("Received credentials: " + userCredentials.getUsername() + ", " + userCredentials.getPassword());
-
-        // Check credentials
-        if ("user".equals(userCredentials.getUsername()) && "admin".equals(userCredentials.getPassword())) {
-            // Return 200 OK for successful login
+        if (authService.login(userCredentials)) {
             return ResponseEntity.ok("Login successful");
         } else {
-            // Return 401 Unauthorized for invalid credentials
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
         }
     }
