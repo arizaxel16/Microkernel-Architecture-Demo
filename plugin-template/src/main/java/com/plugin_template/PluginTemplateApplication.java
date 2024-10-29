57438949e1653ff80d426ada1cc9832179f88ae1
arizaxel16 <arizaxel16@gmail.com>
2024-10-28 19:57:44 -0500

    Add plugin template spring boot

---
+++ plugin-template/src/main/java/com/plugin_template/PluginTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.plugin_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluginTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluginTemplateApplication.class, args);
+	}
+
+}
