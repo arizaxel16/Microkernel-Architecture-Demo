472dd3c4c89da5a97a2e45f831bf237d0072efbf
arizaxel16 <arizaxel16@gmail.com>
2024-10-31 20:28:57 -0500

    Add logging in plugin manager

--- demo/kernel/src/main/java/com/core/plugin/PluginManager.java
+++ demo/kernel/src/main/java/com/core/plugin/PluginManager.java
@@ -1,5 +1,7 @@
 package com.core.plugin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,7 @@ import java.util.jar.JarFile;
 @Component
 @Configuration
 public class PluginManager {
+    private static final Logger logger = LoggerFactory.getLogger(PluginManager.class);
     private final Map<String, Plugin> loadedPlugins = new HashMap<>();
 
     @Value("${plugins.directory:plugins}")
@@ -21,26 +24,33 @@ public class PluginManager {
 
     @PostConstruct
     public void init() {
+        logger.info("Initializing PluginManager...");
+        logger.info("Plugins directory: {}", new File(pluginsDirectory).getAbsolutePath());
         loadPlugins();
+        logLoadedPlugins();
     }
 
     private void loadPlugins() {
         File pluginsDir = new File(pluginsDirectory);
         if (!pluginsDir.exists()) {
+            logger.info("Creating plugins directory at: {}", pluginsDir.getAbsolutePath());
             pluginsDir.mkdirs();
             return;
         }
 
         File[] jarFiles = pluginsDir.listFiles((dir, name) -> name.endsWith(".jar"));
-        if (jarFiles == null)
+        if (jarFiles == null) {
+            logger.warn("No plugin JAR files found in directory: {}", pluginsDir.getAbsolutePath());
             return;
+        }
 
+        logger.info("Found {} potential plugin files", jarFiles.length);
         for (File jar : jarFiles) {
             try {
+                logger.info("Attempting to load plugin from: {}", jar.getName());
                 loadPlugin(jar);
             } catch (Exception e) {
-                System.err.println("Failed to load plugin: " + jar.getName());
-                e.printStackTrace();
+                logger.error("Failed to load plugin: " + jar.getName(), e);
             }
         }
     }
@@ -57,10 +67,12 @@ public class PluginManager {
                         Class<?> clazz = classLoader.loadClass(className);
 
                         if (Plugin.class.isAssignableFrom(clazz) && !clazz.isInterface()) {
+                            logger.info("Found plugin class: {}", className);
                             Plugin plugin = (Plugin) clazz.getDeclaredConstructor().newInstance();
                             registerPlugin(plugin);
                         }
-                    } catch (Exception ignored) {
+                    } catch (Exception e) {
+                        logger.debug("Skipping non-plugin class: {}", entry.getName());
                     }
                 }
             });
@@ -68,16 +80,32 @@ public class PluginManager {
     }
 
     public void registerPlugin(Plugin plugin) {
+        logger.info("Registering plugin: {} (version {})", plugin.getName(), plugin.getVersion());
         plugin.init();
         loadedPlugins.put(plugin.getName(), plugin);
         plugin.start();
+        logger.info("Plugin {} successfully started", plugin.getName());
     }
 
     public void unregisterPlugin(String pluginName) {
         Plugin plugin = loadedPlugins.get(pluginName);
         if (plugin != null) {
+            logger.info("Unregistering plugin: {}", pluginName);
             plugin.stop();
             loadedPlugins.remove(pluginName);
+            logger.info("Plugin {} successfully unregistered", pluginName);
+        } else {
+            logger.warn("Attempted to unregister non-existent plugin: {}", pluginName);
+        }
+    }
+
+    private void logLoadedPlugins() {
+        if (loadedPlugins.isEmpty()) {
+            logger.info("No plugins currently loaded");
+        } else {
+            logger.info("Currently loaded plugins:");
+            loadedPlugins.forEach((name, plugin) -> 
+                logger.info("- {} (version {})", name, plugin.getVersion()));
         }
     }
 
